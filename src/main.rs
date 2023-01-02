8eb656070e2a7a0fc9d32da5cfbf51ac684d1881
SivaKrishna <shivataraya@gmail.com>
2023-01-02 17:15:33 +0530

    add log support to subscribe

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 use std::net::TcpListener;
 
+use env_logger::Env;
 use sqlx::PgPool;
 use zero2prod::{configuration::get_configuration, startup::run};
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
+    env_logger::Builder::from_env(Env::default().default_filter_or("trace")).init();
     let configuration = get_configuration().expect("Failed to read configuration.");
     let connection_pool = PgPool::connect(&configuration.database.connection_string())
         .await
